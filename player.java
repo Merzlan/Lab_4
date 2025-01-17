4d5d80263c36d50f7714dd6543eb5d0aba4a5691
Merzlan <kostya123qwertyzubenko4.0@gmail.com>
2025-01-18 05:35:59 +0700

    Добавлена демонстрация статического поля и статического метода для подсчета кол-ва созданных игроков и показа использования оператора this. Добавлены комментарии

--- player.java
+++ player.java
@@ -4,17 +4,28 @@ public class player {
     private int level;
     private int attackPower;
 
+    private static int playerCount = 0; // Статическое поле для подсчета игроков
+
     public player(String name, int health, int level, int attackPower) {
         this.name = name;
         this.health = health;
         this.level = level;
         this.attackPower = attackPower;
+        playerCount++; // Увеличиваем счетчик игроков
+    }
+
+    public static int getPlayerCount() {
+        return playerCount; // Статический метод для получения количества игроков
     }
 
     public void printInfo() {
-        System.out.println("Персонаж: " + name +
-                           "\nЗдоровье: " + health +
-                           "\nУровень: " + level +
-                           "\nСила атаки: " + attackPower);
+        System.out.println("Персонаж: " + this.name + // Использование this для ссылки на текущий объект
+                           "\nЗдоровье: " + this.health +
+                           "\nУровень: " + this.level +
+                           "\nСила атаки: " + this.attackPower);
+    }
+
+    public String getName() {
+        return this.name; // Использование this
     }
 }
